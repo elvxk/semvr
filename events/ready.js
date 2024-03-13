431ba39bcb1d70ec6ef73c996c17d6980198e6d4
elvxk <pradiptaramadan9c@gmail.com>
2024-03-13 11:20:37 +0700

    feat:activity status

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,30 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
+    const activity = [
+      "🌼 Punokawan",
+      "🌼 Pandawa",
+      "🌼 SEMVR V1",
+      "🌼 Karangkadempel",
+    ];
+    let enuma = 0;
+    setInterval(() => {
+      if (enuma < activity.length - 1) {
+        enuma++;
+      } else {
+        enuma = 0;
+      }
+      client.user.setActivity(activity[enuma], {
+        type: ActivityType.Watching,
+      });
+    }, 5000);
+    const semvr =
+      "  ___  ___ _ __ _____   ___ __ \r\n / __|/ _ \\ '_ ` _ \\ \\ / / '__|\r\n \\__ \\  __/ | | | | \\ V /| |   \r\n |___/\\___|_| |_| |_|\\_/ |_|   \r\n";
+    console.log(semvr);
+    console.log(` 🚀 Ready! Logged in as ${client.user.tag}`);
+    console.log(` 🌼 Watching ${client.guilds.cache.size} server`);
   },
 };
